f21e982ad638bd1c976e4238fda67ebbd7ee8f91
Rohit Singh <rohit.singh@rts.com.np>
2022-11-28 10:08:22 +0000

    need to debug

--- User/Src/main.c
+++ User/Src/main.c
@@ -14,6 +14,8 @@
 #include "ble_controller.h"
 #include "rf_driver_ll_bus.h"
 
+#include "gap_profile.h"
+
 
 /* FreeRTOS includes. */
 #include "FreeRTOS.h"
@@ -22,91 +24,133 @@
 #include "semphr.h"
 #include "freertos_ble.h"
 
+#define DEVICE_IDLE_STATE         0
+#define DEVICE_ADV_STATE          1
+#define DEVICE_CONN_STATE         2
+#define DEVICE_SLEEP_STATE        3
+#define DEVICE_DISCONNECTED_STATE 4
+
+NO_INIT(uint32_t dyn_alloc_a[DYNAMIC_MEMORY_SIZE>>2]);
+
 SemaphoreHandle_t radioActivitySemaphoreHandle;
 SemaphoreHandle_t BLETickSemaphoreHandle;
 
+static uint8_t DeviceState;
+static uint16_t ConnectionInterval;
 
-#define BLE_BEACON_VERSION_STRING "1.1"
+static uint8_t AdvertisementData[] = { 0x02, AD_TYPE_FLAGS, FLAG_BIT_LE_GENERAL_DISCOVERABLE_MODE | FLAG_BIT_BR_EDR_NOT_SUPPORTED,
+	6, AD_TYPE_COMPLETE_LOCAL_NAME, 'R', '-', 'B', 'L', 'E' };
 
-/* Advertising interval for legacy advertising (0.625 ms units) 
-  For iBeacon this should be set to 100 ms. */
-#define LEGACY_ADV_INTERVAL     160  /* 100 ms */
-#define EXT_ADV_INTERVAL        160 /* 100 ms */
-
-/* Set to 1 to enable the name AD data in extended advertising events (if
-  extended advertising events are used).  */
-#define DEVICE_NAME_IN_ADV 0
-
-/* PHY used in extended advertising events. One between: LE_1M_PHY,
-  LE_2M_PHY and LE_CODED_PHY.  */
-#define EXT_ADV_PHY LE_CODED_PHY
-
-/*-----------------------------------------------------------*/
-/* Wait time of the test task (numbe rof ticks) */
-#define TEST_PERIOD         			    ( 200 / portTICK_PERIOD_MS )
-#define ADV_CHANGE_PERIOD         			( 500 / portTICK_PERIOD_MS )
-
-/* This is the length of advertising data to be set for legacy advertising.
-   It does not include the device name, which is sent only in extended
-  advertising events. */
-#define SHORT_ADV_DATA_LENGTH    27
-
-static uint8_t adv_data[] = {
-  /* Advertising data: Flags AD Type not supported if broadcaster only */
-  /* 0x02, 0x01, 0x06, */
-  /* Advertising data: manufacturer specific data */
-  26, //len
-  AD_TYPE_MANUFACTURER_SPECIFIC_DATA,  //manufacturer type
-  0x4C, 0x00, //Company identifier code
-  0x02,       // ID
-  0x15,       //Length of the remaining payload
-  0xE2, 0x0A, 0x39, 0xF4, 0x73, 0xF5, 0x4B, 0xC4, //Location UUID
-  0xA1, 0x2F, 0x17, 0xD1, 0xAD, 0x07, 0xA9, 0x61,
-  0x00, 0x05, // Major number 
-  0x00, 0x07, // Minor number 
-  (uint8_t)-56,         // Tx power measured at 1 m of distance (in dBm)
-#if DEVICE_NAME_IN_ADV
-  15,       // Length of following AD data
-  0x09,'E','x','t','e','n','d','e','d','B','e','a','c','o','n'
-#endif
-};
-
-
-FILE __stdout;
-FILE __stdin;
-
-/* KEIL fputc implementation template allowing to redirect printf output towards serial port (UART/USB) */
-int fputc(int c, FILE *f)
-{  
-//  BSP_COM_Write((uint8_t *)&c, 1);
+static Advertising_Set_Parameters_t AdvertisingSetParamters[1];
+	
+void InitDevice(void)
+{
+  uint8_t ret;
+  uint16_t ServiceHandle;
+  uint16_t DeviceNameCharHandle;
+  uint16_t AppearanceCharHandle;
+  uint8_t BLEAddress[CONFIG_DATA_PUBADDR_LEN] = {0x67,0x77,0x88,0xE1,0x80,0x02};
+	uint8_t DeviceName[] = { 'R', '-', 'B', 'L', 'E' };
   
-  return 1;
+  ret = aci_hal_write_config_data(CONFIG_DATA_PUBADDR_OFFSET, CONFIG_DATA_PUBADDR_LEN, BLEAddress);
+	
+	if (ret != BLE_STATUS_SUCCESS)
+	{
+		// Error
+	}
+	else
+	{
+		// Success
+	}
+  
+	ret = aci_hal_set_tx_power_level(0, 24); // 24 = 0dbm transmit power
+	
+  if(ret != BLE_STATUS_SUCCESS) 
+	{
+//  Error
+    while(1); // go to limbo
+  }
+
+  ret = aci_gatt_srv_init();
+	if (ret != 	BLE_STATUS_SUCCESS)
+	{
+		// Error
+	}
+	
+	ret = aci_gap_init(GAP_PERIPHERAL_ROLE, 0, sizeof DeviceName, 0 , &ServiceHandle, &DeviceNameCharHandle, &AppearanceCharHandle);
+  if (ret != BLE_STATUS_SUCCESS)
+  {
+//    Error
+  }
+  else
+  {
+//    Success
+  }
+	
+	ret = Gap_profile_set_dev_name(0, sizeof DeviceName, DeviceName);
+	if (ret != BLE_STATUS_SUCCESS)
+  {
+//    Error
+  }
+  else
+  {
+//    Success
+  }
 }
 
-
-int fgetc (FILE *f) {
-  int data = 0;
-//  while (Read_Buffer_Pop((uint8_t *)&data) != SUCCESS);
-  return data;
+void ConfigureDeviceAdvertising (uint16_t AdvertisingInterval)
+{
+	uint8_t ret;
+	
+	ret = aci_gap_set_advertising_configuration(0, GAP_MODE_GENERAL_DISCOVERABLE, ADV_PROP_CONNECTABLE | ADV_PROP_SCANNABLE | ADV_PROP_LEGACY,
+		(AdvertisingInterval * 1000) / 625, (AdvertisingInterval * 1000) / 625,
+		ADV_CH_ALL,
+		PUBLIC_ADDR, NULL,
+		ADV_NO_WHITE_LIST_USE,
+		0,
+		LE_1M_PHY,
+		0,
+		LE_1M_PHY,
+		0,
+		0);
+	
+	ret = aci_gap_set_advertising_data(0, ADV_COMPLETE_DATA,  sizeof AdvertisementData, AdvertisementData);
 }
 
-
-NO_INIT(uint32_t dyn_alloc_a[DYNAMIC_MEMORY_SIZE>>2]);
+void SetDeviceConnectable (void)
+{
+	uint8_t ret;
+	
+	AdvertisingSetParamters[0].Advertising_Handle = 0;
+	AdvertisingSetParamters[0].Duration = 0;
+	AdvertisingSetParamters[0].Max_Extended_Advertising_Events = 0;
+	
+	ret = aci_gap_set_advertising_enable(ENABLE, 1, AdvertisingSetParamters);
+	if (ret != BLE_STATUS_SUCCESS)
+	{
+		// Error
+	}
+	else
+	{
+		// Success
+		DeviceState = DEVICE_ADV_STATE;
+	}
+}
 
 void InitModules(void)
 {
   uint8_t ret;
-  BLE_STACK_InitTypeDef BLE_STACK_InitParams = BLE_STACK_INIT_PARAMETERS;
+  BLE_STACK_InitTypeDef BLEStackInitParams = BLE_STACK_INIT_PARAMETERS;
   
   LL_AHB_EnableClock(LL_AHB_PERIPH_PKA|LL_AHB_PERIPH_RNG);
-
   
   BLECNTR_InitGlobal();
   
-  HAL_VTIMER_InitType VTIMER_InitStruct = {HS_STARTUP_TIME, INITIAL_CALIBRATION, CALIBRATION_INTERVAL};
-  HAL_VTIMER_Init(&VTIMER_InitStruct);
+  HAL_VTIMER_InitType VTIMERInitStruct = {HS_STARTUP_TIME, INITIAL_CALIBRATION, CALIBRATION_INTERVAL};
+  HAL_VTIMER_Init(&VTIMERInitStruct);
   
   BLEPLAT_Init();  
+	
   if (PKAMGR_Init() == PKAMGR_ERROR)
   {
       while(1);
@@ -116,210 +160,50 @@ void InitModules(void)
       while(1);
   }
   
-  /* Init the AES block */
   AESMGR_Init();
   
-  /* BlueNRG-LP stack init */
-  ret = BLE_STACK_Init(&BLE_STACK_InitParams);
+  ret = BLE_STACK_Init(&BLEStackInitParams);
   if (ret != BLE_STATUS_SUCCESS) {
-    printf("Error in BLE_STACK_Init() 0x%02x\r\n", ret);
+    // Error
     while(1);
   }
-  
 }
 
 void ModulesTick(void)
 {
-  /* Timer tick */
   HAL_VTIMER_Tick();
-  
-  /* Bluetooth stack tick */
   BLE_STACK_Tick();
-  
-  /* NVM manager tick */
   NVMDB_Tick();
 }
 
-void InitDevice(void)
-{
-  uint8_t ret;
-  uint16_t service_handle;
-  uint16_t dev_name_char_handle;
-  uint16_t appearance_char_handle;
-  uint8_t address[CONFIG_DATA_PUBADDR_LEN] = {0x66,0x77,0x88,0xE1,0x80,0x02};
-  
-  aci_hal_write_config_data(CONFIG_DATA_PUBADDR_OFFSET, CONFIG_DATA_PUBADDR_LEN, address);
-  
-  /* Set the TX Power to 0 dBm */
-  ret = aci_hal_set_tx_power_level(0, 24);
-  if(ret != 0) {
-//    PRINTF ("Error in aci_hal_set_tx_power_level() 0x%04xr\n", ret);
-    while(1);
-  }
+//static void BLETask (void * params)
+//{
+//	WakeupSourceConfig_TypeDef wakeupIO;
+//	PowerSaveLevels stopLevel;
+//	
+////  xSemaphoreTake(BLETickSemaphoreHandle, portMAX_DELAY);
+//  
+//  InitDevice();
+//  
+////  StartBeacon();
+//  
+////  xSemaphoreGive(BLETickSemaphoreHandle);
+//  
+//  while(1)
+//  {
+////    xSemaphoreTake(BLETickSemaphoreHandle, portMAX_DELAY);
+//    ModulesTick();    
+////    xSemaphoreGive(BLETickSemaphoreHandle);
+//		
+////    if(BLE_STACK_SleepCheck() != POWER_SAVE_LEVEL_RUNNING && HAL_VTIMER_SleepCheck() == TRUE)
+////    {
+////      xSemaphoreTake(radioActivitySemaphoreHandle, portMAX_DELAY);
+////    }
+//		
+//		HAL_PWR_MNGR_Request(POWER_SAVE_LEVEL_STOP_WITH_TIMER, wakeupIO, &stopLevel);
+//  }
+//}
 
-  
-  /* Init the GAP: broadcaster role */
-  ret = aci_gap_init(GAP_BROADCASTER_ROLE, 0x00, 0x08, PUBLIC_ADDR, &service_handle, &dev_name_char_handle, &appearance_char_handle);
-  if (ret != 0)
-  {
-//    PRINTF ("Error in aci_gap_init() 0x%04x\r\n", ret);
-  }
-  else
-  {
-//    PRINTF ("aci_gap_init() --> SUCCESS\r\n");
-  }
-}
-
-static void StartBeacon (void)
-{
-	uint8_t ret;
-  Advertising_Set_Parameters_t Advertising_Set_Parameters[2];
-  uint8_t adv_sets = 0;
-  
-#ifdef LEGACY_ADV
-   
-  /* Set advertising configuration for legacy advertising in broadcast mode */  
-  ret = aci_gap_set_advertising_configuration(0, GAP_MODE_BROADCAST,
-                                              ADV_PROP_LEGACY,
-                                              LEGACY_ADV_INTERVAL, LEGACY_ADV_INTERVAL,
-                                              ADV_CH_ALL,
-                                              0,NULL, /* No peer address */
-                                              ADV_NO_WHITE_LIST_USE,
-                                              0, /* 0 dBm */
-                                              LE_1M_PHY, /* Primary advertising PHY */
-                                              0, /* 0 skips */
-                                              LE_1M_PHY, /* Secondary advertising PHY. Not used with legacy advertising. */
-                                              0, /* SID */
-                                              0 /* No scan request notifications */);
-  if (ret != BLE_STATUS_SUCCESS)
-  {
-//    PRINTF("Error in aci_gap_set_advertising_configuration() 0x%02x\r\n", ret);
-    return;
-  }
-  
-  ret = aci_gap_set_advertising_data(0, ADV_COMPLETE_DATA, SHORT_ADV_DATA_LENGTH, adv_data);
-  if (ret != BLE_STATUS_SUCCESS)
-  {
-//    PRINTF("Error in aci_gap_set_advertising_data() 0x%02x\r\n", ret);
-    return;
-  }
-  
-  printf("Legacy advertising configured\n");
-  
-  Advertising_Set_Parameters[adv_sets].Advertising_Handle = 0;
-  Advertising_Set_Parameters[adv_sets].Duration = 0;
-  Advertising_Set_Parameters[adv_sets].Max_Extended_Advertising_Events = 0;
-  
-  adv_sets++;
-  
-#endif
-  
-#ifdef EXTENDED_ADV
-  /* Set advertising configuration for extended advertising in broadcast mode */  
-  ret = aci_gap_set_advertising_configuration(1, GAP_MODE_BROADCAST,
-                                              ADV_PROP_NONE,
-                                              EXT_ADV_INTERVAL, EXT_ADV_INTERVAL,
-                                              ADV_CH_ALL,
-                                              0,NULL, /* No peer address */
-                                              ADV_NO_WHITE_LIST_USE,
-                                              0, /* 0 dBm */
-                                              (EXT_ADV_PHY==LE_2M_PHY)?LE_1M_PHY:EXT_ADV_PHY, /* Primary advertising PHY */
-                                              0, /* 0 skips */
-                                              EXT_ADV_PHY, /* Secondary advertising PHY */
-                                              0, /* SID */
-                                              0 /* No scan request notifications */);
-  if (ret != BLE_STATUS_SUCCESS)
-  {
-//    PRINTF("Error in aci_gap_set_advertising_configuration() 0x%02x\r\n", ret);
-    return;
-  }
-  
-  ret = aci_gap_set_advertising_data(1, ADV_COMPLETE_DATA, sizeof(adv_data), adv_data);
-  if (ret != BLE_STATUS_SUCCESS)
-  {
-//    PRINTF("Error in aci_gap_set_advertising_data() 0x%02x\r\n", ret);
-    return;
-  }
-  
-  printf("Extended advertising configured\n");
-  
-  Advertising_Set_Parameters[adv_sets].Advertising_Handle = 1;
-  Advertising_Set_Parameters[adv_sets].Duration = 0;
-  Advertising_Set_Parameters[adv_sets].Max_Extended_Advertising_Events = 0;
-  
-  adv_sets++;
-  
-#endif /* EXTENDED_ADV */
-
-  /* Enable advertising */
-  ret = aci_gap_set_advertising_enable(ENABLE, adv_sets, Advertising_Set_Parameters);
-  if (ret != BLE_STATUS_SUCCESS)
-  {
-//    PRINTF ("Error in aci_gap_set_advertising_enable() 0x%02x\r\n", ret);
-    return;
-  }
-  
-  printf("Advertising started\n");
-}
-
-static void BLETask (void * params)
-{
-  xSemaphoreTake(BLETickSemaphoreHandle, portMAX_DELAY);
-  
-  InitDevice();
-  
-  StartBeacon();
-  
-  xSemaphoreGive(BLETickSemaphoreHandle);
-  
-  while(1)
-  {
-    xSemaphoreTake(BLETickSemaphoreHandle, portMAX_DELAY);
-    ModulesTick();    
-    xSemaphoreGive(BLETickSemaphoreHandle);
-		
-    if(BLE_STACK_SleepCheck() != POWER_SAVE_LEVEL_RUNNING && HAL_VTIMER_SleepCheck() == TRUE)
-    {
-      xSemaphoreTake(radioActivitySemaphoreHandle, portMAX_DELAY);
-    }
-  }
-}
-
-static void changeADVDataTask( void *pvParameters )
-{
-  TickType_t xNextWakeTime;
-  uint8_t Random_Number[8];
-  
-  /* Initialise xNextWakeTime - this only needs to be done once. */
-  xNextWakeTime = xTaskGetTickCount();
-  
-  for( ;; )
-  {
-    /* Place this task in the blocked state until it is time to run again.
-    The block time is specified in ticks, the constant used converts ticks
-    to ms.  While in the Blocked state this task will not consume any CPU
-    time. */
-    vTaskDelayUntil( &xNextWakeTime, ADV_CHANGE_PERIOD );
-    
-    BLE_ACI_PROTECTED(hci_le_rand(Random_Number));
-    
-    adv_data[25] = Random_Number[0];
-    
-    /* In this case there is no need to disable advertising before updating buffer
-       content, since only one byte is changed (atomic operation) and there is no
-       risk to have inconsistent data. */
-    
-    BLE_ACI_PROTECTED(aci_gap_set_advertising_data(0, ADV_COMPLETE_DATA, SHORT_ADV_DATA_LENGTH, adv_data));
-    
-    
-#if EXTENDED_ADV
-    BLE_ACI_PROTECTED(aci_gap_set_advertising_data(1, ADV_COMPLETE_DATA, sizeof(adv_data), adv_data));
-#endif
-    
-//    PRINTF("ADV change %d\r\n", adv_data[25]);
-    
-  }  
-}
 
 void initIO (void)
 {
@@ -357,30 +241,106 @@ void initIO (void)
 }
 
 int main (void) {
-//	vTaskDelay(3000); // safety stuff
 	
-	while (SystemInit(SYSCLK_64M, BLE_SYSCLK_32M) != 0) {
+	WakeupSourceConfig_TypeDef WakeupIO;
+	PowerSaveLevels StopLevel;
+	
+	while (SystemInit(SYSCLK_16M, BLE_SYSCLK_16M) != 0) {
 		vTaskDelay(1000);
 	};
 	
-	radioActivitySemaphoreHandle = xSemaphoreCreateBinary();
-	BLETickSemaphoreHandle = xSemaphoreCreateMutex();
+//	radioActivitySemaphoreHandle = xSemaphoreCreateBinary();
+//	BLETickSemaphoreHandle = xSemaphoreCreateMutex();
 	
-	if (radioActivitySemaphoreHandle==NULL || BLETickSemaphoreHandle == NULL){
-    while(1) {
-//			vTaskDelay(1000); // go to limbo
-		}
-  }
+//	if (radioActivitySemaphoreHandle==NULL || BLETickSemaphoreHandle == NULL){
+//    while (1);
+//  }
 	
-	initIO();
-    
-  InitModules(); 
+//	initIO();
+  InitModules();
+	InitDevice();
 	
-	xTaskCreate(BLETask, "BLEStack", 650, NULL, tskIDLE_PRIORITY + 1, NULL);
-  xTaskCreate(changeADVDataTask, "ADV", 150, NULL, tskIDLE_PRIORITY + 2, NULL );
+	WakeupIO.IO_Mask_High_polarity = 0;
+	WakeupIO.IO_Mask_Low_polarity = 0;
+	WakeupIO.RTC_enable = 0;
+	WakeupIO.LPU_enable = 0;
+	
+	DeviceState = DEVICE_IDLE_STATE;
+	ConfigureDeviceAdvertising(1000);
+	SetDeviceConnectable();
+	DeviceState = DEVICE_ADV_STATE;
+	
+	while (1)
+	{
+		ModulesTick();
+		
+//		HAL_PWR_MNGR_Request(POWER_SAVE_LEVEL_STOP_WITH_TIMER, WakeupIO, &StopLevel);
+	}
+	
+//	HAL_PWR_MNGR_Request(POWER_SAVE_LEVEL_STOP_WITH_TIMER, wakeupIO, &stopLevel);
+	
+//	xTaskCreate(BLETask, "BLEStack", 650, NULL, tskIDLE_PRIORITY + 1, NULL);
   
-  /* Start the tasks and timer running. */
-  vTaskStartScheduler();
+//  vTaskStartScheduler();
 	
 	return 0;
+}
+
+FILE __stdout;
+FILE __stdin;
+
+int fputc(int c, FILE *f)
+{  
+//  BSP_COM_Write((uint8_t *)&c, 1);
+  
+  return 1;
+}
+
+int fgetc (FILE *f) {
+  int data = 0;
+//  while (Read_Buffer_Pop((uint8_t *)&data) != SUCCESS);
+  return data;
+}
+
+PowerSaveLevels App_PowerSaveLevel_Check(PowerSaveLevels level)
+{
+	if(BLE_STACK_SleepCheck() == POWER_SAVE_LEVEL_RUNNING && HAL_VTIMER_SleepCheck() != TRUE)
+	{
+		return POWER_SAVE_LEVEL_RUNNING;
+	}
+  
+  return POWER_SAVE_LEVEL_STOP_WITH_TIMER;
+}
+
+void hci_le_connection_complete_event (uint8_t Status,
+	uint16_t ConnectionHandle,
+	uint8_t Role, uint8_t PeerAddressType, uint8_t PeerAddress[6],
+	uint16_t ConnectionIntervalParam, uint16_t ConnectionLatency, uint16_t SupervisionTimeout,
+	uint8_t MasterClockAccuracy)
+{
+	if (Status == BLE_STATUS_SUCCESS)	
+	{
+		DeviceState = DEVICE_CONN_STATE;
+		ConnectionInterval = ConnectionIntervalParam;
+	}
+}
+
+void hci_le_enhanced_connection_complete_event (uint8_t Status,
+	uint16_t ConnectionHandle,
+	uint8_t Role, uint8_t PeerAddressType, uint8_t PeerAddress[6], uint8_t LocalResolvablePrivateAddress[6], uint8_t PeerResolvablePrivateAddress[6],
+	uint16_t ConnectionIntervalParam, uint16_t ConnectionLatency, uint16_t SupervisionTimeout,
+	uint8_t MasterClockAccuracy)
+{
+	hci_le_connection_complete_event(
+		Status, ConnectionHandle, Role, PeerAddressType,
+		PeerAddress, ConnectionIntervalParam, ConnectionLatency,
+		SupervisionTimeout, MasterClockAccuracy
+	);
+}
+
+void hci_disconnection_complete_event (uint8_t Status,
+	uint16_t ConnectionHandle,
+	uint8_t Reason)
+{
+	DeviceState = DEVICE_DISCONNECTED_STATE;
 }
